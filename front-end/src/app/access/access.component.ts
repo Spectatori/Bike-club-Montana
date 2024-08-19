7121b79bcd2d0c715dbf5749e698c6e73ae59425
Spectatori <cavdar2002@gmail.com>
2024-08-19 14:04:10 +0300

    added the register panel without the fields for writing and without the buttons

--- front-end/src/app/access/access.component.ts
+++ front-end/src/app/access/access.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavComponent} from "../../layouts/nav/nav.component";
+import {SideMenuComponent} from "../../layouts/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-access',
   standalone: true,
   imports: [
-    NavComponent
+    NavComponent,
+    SideMenuComponent
   ],
   templateUrl: './access.component.html',
   styleUrl: './access.component.css'
