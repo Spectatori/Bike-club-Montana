9856850c862760e786b2834e8a15a1288f7edfe6
Spectatori <cavdar2002@gmail.com>
2024-08-19 13:45:17 +0300

    finished contacts page and deleted unused code in home css and html

--- front-end/src/app/contacts/contacts.component.ts
+++ front-end/src/app/contacts/contacts.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NavComponent} from "../../layouts/nav/nav.component";
+import {SideMenuComponent} from "../../layouts/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-    imports: [
-        NavComponent
-    ],
+  imports: [
+    NavComponent,
+    SideMenuComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
