10d34b330f4a102bfaff8be8b78c5a677a883ce1
Spectatori <cavdar2002@gmail.com>
2024-08-19 13:24:37 +0300

    fixed side menu and finished about page

--- front-end/src/app/about/about.component.ts
+++ front-end/src/app/about/about.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {NavComponent} from "../../layouts/nav/nav.component";
+import {NgOptimizedImage} from "@angular/common";
+import {SideMenuComponent} from "../../layouts/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    NavComponent
+    NavComponent,
+    NgOptimizedImage,
+    SideMenuComponent
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
