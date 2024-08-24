c171b8431af86ee16eae79b5b411eaaadfc045ba
Spectatori <cavdar2002@gmail.com>
2024-08-24 15:36:12 +0300

    access page finished visually

--- front-end/src/app/access/access.component.ts
+++ front-end/src/app/access/access.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import {NavComponent} from "../../layouts/nav/nav.component";
 import {SideMenuComponent} from "../../layouts/side-menu/side-menu.component";
+import {NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-access',
   standalone: true,
   imports: [
     NavComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    NgIf,
+    FormsModule
   ],
   templateUrl: './access.component.html',
   styleUrl: './access.component.css'
 })
 export class AccessComponent {
-
+  accessIndex = 0;
+  AccessChange(){
+    this.accessIndex = (this.accessIndex + 1) % 2
+    console.log(this.accessIndex)
+  }
+  user = {
+    username:'',
+    password:'',
+    email:'',
+  }
 }
